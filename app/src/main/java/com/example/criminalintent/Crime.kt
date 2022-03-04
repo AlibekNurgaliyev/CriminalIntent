33a1535b2ef80cfbd4022a85034310b6fc4d3f15
alibeknurgaliyev <a.nurgaliyev@mycar.kz>
2022-03-04 18:24:38 +0600

    add fragment

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -3,8 +3,8 @@ package com.example.criminalintent
 import java.util.*
 
 data class Crime(
-    val id: UUID = UUID.randomUUID(),
-    val title: String = "",
-    val date: Date = Date(),
-    val isSolved: Boolean = false
+    var id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false
 )
