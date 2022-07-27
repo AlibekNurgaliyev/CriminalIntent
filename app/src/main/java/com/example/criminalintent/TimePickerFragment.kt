9abdada4f3e9ac3e8343453124481e493426ac18
alibeknurgaliyev <a.nurgaliyev@mycar.kz>
2022-07-27 18:52:19 +0600

    added menu bar

--- app/src/main/java/com/example/criminalintent/TimePickerFragment.kt
+++ app/src/main/java/com/example/criminalintent/TimePickerFragment.kt
@@ -5,9 +5,7 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
-import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.math.min
 
 class TimePickerFragment : DialogFragment() {
 
@@ -15,16 +13,11 @@ class TimePickerFragment : DialogFragment() {
         val calender = Calendar.getInstance()
         val timeListener =
             TimePickerDialog.OnTimeSetListener { _: TimePicker, hours: Int, minutes: Int ->
-
-//                val Ho = calender.set(Calendar.HOUR, hours)
-//                val Ho1 = calender.set(Calendar.MINUTE, minutes)
-
                 val resultDate: Date = GregorianCalendar(0,0,0,hours,minutes).time
 
                 targetFragment?.let { fragment ->
                     (fragment as Callbacks).onTimeSelected(resultDate)
                 }
-
             }
 
         val date = arguments?.getSerializable(ARG_TIME) as Date
@@ -32,7 +25,6 @@ class TimePickerFragment : DialogFragment() {
         calender.time = date
         val initialHours = calender.get(Calendar.HOUR)
         val initialMinutes = calender.get(Calendar.MINUTE)
-
 
         return TimePickerDialog(
             requireContext(),
@@ -43,20 +35,14 @@ class TimePickerFragment : DialogFragment() {
         )
     }
 
-    fun convertLongToTime(time: Long): String {
-        val date = Date(time)
-        val format = SimpleDateFormat("HH:mm")
-        return format.format(date)
-    }
-
     companion object {
         private const val ARG_TIME = "time"
 
         fun newInstance(time: Date): TimePickerFragment {
             val args = Bundle().apply {
                 putSerializable(ARG_TIME, time)
-
             }
+
             return TimePickerFragment().apply {
                 arguments = args
             }
